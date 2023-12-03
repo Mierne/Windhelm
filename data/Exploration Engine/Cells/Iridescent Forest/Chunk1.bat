812c548e338f0b3c4affec60a13751b286d671eb
Mierne <152667836+Mierne@users.noreply.github.com>
2023-12-03 00:21:36 -0500

    Upated exploration mechanic.
    New, less complicated method of exploring the cell.

--- data/Exploration Engine/Cells/Iridescent Forest/Chunk1.bat
+++ data/Exploration Engine/Cells/Iridescent Forest/Chunk1.bat
@@ -1,169 +1,78 @@
 @ECHO OFF
 TITLE Iridescent Forest (EXPLORE) - Chunk 1
-REM Chunk 1 (231201)
+REM Chunk 1 (231202)
 REM --
 
-REM There are 5 posible positions in each chunk and 4 "directions" in each position, each "direction" can be explored 3 times.
-REM When a savegame is loaded and the Player explores,
-REM We need to find the chunk and position they were last in.
-:POS_CHECK
-IF %LC% EQU 1 (
-    IF %CPD% EQU 1 (
-        REM 1 EQU North.
-        GOTO :ChunkPos1N
-    ) ELSE IF %CPD% EQU 2 (
-        REM 2 EQU East.
-        GOTO :ChunkPos1E
-    ) ELSE IF %CPD% 3 (
-        REM 3 EQU South.
-        GOTO :ChunkPos1S
-    ) ELSE IF %CPD% 4 (
-        REM 4 EQU West.
-        GOTO :ChunkPos1W
-    )
-) ELSE IF %LC% EQU 2 (
-    IF %CPD% EQU 1 (
-        REM 1 EQU North.
-        GOTO :ChunkPos2N
-    ) ELSE IF %CPD% EQU 2 (
-        REM 2 EQU East.
-        GOTO :ChunkPos2E
-    ) ELSE IF %CPD% 3 (
-        REM 3 EQU South.
-        GOTO :ChunkPos2S
-    ) ELSE IF %CPD% 4 (
-        REM 4 EQU West.
-        GOTO :ChunkPos2W
-    )
-) ELSE IF %LC% EQU 3 (
-    IF %CPD% EQU 1 (
-        REM 1 EQU North.
-        GOTO :ChunkPos3N
-    ) ELSE IF %CPD% EQU 2 (
-        REM 2 EQU East.
-        GOTO :ChunkPos3E
-    ) ELSE IF %CPD% 3 (
-        REM 3 EQU South.
-        GOTO :ChunkPos3S
-    ) ELSE IF %CPD% 4 (
-        REM 4 EQU West.
-        GOTO :ChunkPos3W
-    )
-) ELSE IF %LC% EQU 4 (
-    IF %CPD% EQU 1 (
-        REM 1 EQU North.
-        GOTO :ChunkPos4N
-    ) ELSE IF %CPD% EQU 2 (
-        REM 2 EQU East.
-        GOTO :ChunkPos4E
-    ) ELSE IF %CPD% 3 (
-        REM 3 EQU South.
-        GOTO :ChunkPos4S
-    ) ELSE IF %CPD% 4 (
-        REM 4 EQU West.
-        GOTO :ChunkPos4W
-    )
-) ELSE IF %LC% EQU 5 (
-    IF %CPD% EQU 1 (
-        REM 1 EQU North.
-        GOTO :ChunkPos5N
-    ) ELSE IF %CPD% EQU 2 (
-        REM 2 EQU East.
-        GOTO :ChunkPos5E
-    ) ELSE IF %CPD% 3 (
-        REM 3 EQU South.
-        GOTO :ChunkPos5S
-    ) ELSE IF %CPD% 4 (
-        REM 4 EQU West.
-        GOTO :ChunkPos5W
-    )
-) ELSE (
-    REM error
-    ECHO errorType=ChunkPos. >> log.txt
-    EXIT /B
-)
+REM Chunk 1 contains low-level enemies, such as the Bandit and Jester.
+REM This chunk also contains several NPCs important to the story, and will help guide the Player.
 
-REM "Main" area of a Chunk Direction.
-:ChunkPos1N
-SET LC=ChunkPos1N
+REM Main screen, like a hub of sorts.
+:MAIN
+SET LC=MAIN
 CLS
 ECHO.
 TYPE "TITLE\PATH\HERE"
 ECHO.
-ECHO DEBUG: movement in this direction left: %C1P1NM%
-ECHO Where would you like to go?
 ECHO %displayMessage%
 ECHO =====================================================================================================
-ECHO [1 / GO NORTH ] ^| [2 / GO SOUTH ] ^|[3 / GO EAST ] ^| [4 / GO WEST ] ^| [E / GO BACK ]
-CHOICE /C 1234E /N /M ">"
-IF ERRORLEVEL 5 GOTO :AUTOSAVE
-IF ERRORLEVEL 4 GOTO :C1Wc
-IF ERRORLEVEL 3 GOTO :C1Ec
-IF ERRORLEVEL 2 GOTO :C1Sc
-IF ERRORLEVEL 1 GOTO :C1Nc
+ECHO [1 / EXPLORE ] ^| [ I / INVENTORY ] ^| [E / GO BACK ]
+CHOICE /C 1IE /N /M ">"
+IF ERRORLEVEL 3 GOTO :AUTOSAVE
+IF ERRORLEVEL 2 GOTO :INVENTORY
+IF ERRORLEVEL 1 GOTO :MOVEMENT_CHECK
 
-REM North movement point check.
-REM If zero, direction is blocked, otherwise continue.
-:C1Nc
-IF %C1P1NM% GTR 0 (
-    SET /A C1P1NM=!C1P1NM! -1
-    SET displayMessage=Moved North.
-    SET LC=C1P1N
-    GOTO :C1P1Nencheck
+REM Checks the Players HP to prevent instant-death in an encounter.
+:MOVEMENT_CHECK
+IF %HP% LSS 50 (
+    GOTO :HPTL
 ) ELSE (
-    SET displayMessage=Your path is blocked.
-    GOTO :%LC%
+    GOTO :ENCOUNTER_CHECK
 )
 
-REM Encounter check. Will the Player encounter an enemy, NPC or item?
-GOTO :C1P1Nencheck
-SET /A C1E=%RANDOM% %%10
-IF %C1E% LSS 2 (
-    REM Ultra rare loot.
-    SET displayMessage=Discovered a chest full of Gold!
-    SET /A COINS=!COINS! +2000
-    GOTO :%LC%
-) ELSE IF %C1E% GTR 9 (
-    REM Found nothing.
-    SET displayMessage=You find nothing but more... Forest.
-    GOTO :%LC%
-) ELSE IF %C1E% GTR 6 (
-    REM Bandit Encounter.
-    SET displayMessage=A Bandit appears before you, blocking your path.
-    set currentEnemy=iBandit
-    CALL "%cd%\data\functions\Combat Engine\scripts\var_handler.bat"
-    GOTO :%LC%
-) ELSE IF %C1E% GTR 2 (
-    REM Common loot.
-    SET displayMessage=Found 13 Gold!
+REM Does the Player encounter an enemy or NPC?
+:ENCOUNTER_CHECK
+SET /A E=%RANDOM% %%6
+IF %E% EQU 1 (
+    REM Jester Encounter
+    SET currentEnemy=iJester
+    CALL "%cd%\data\Combat Engine\scripts\var_handler.bat"
+    IF %HP% LEQ 0 (
+        SET displayMessage=You wake up and find yourself safe within the walls of Windhelm.
+        GOTO :AUTOSAVE
+    ) ELSE (
+        GOTO :MAIN
+    )
+) ELSE IF %E% EQU 2 (
+    REM Bandit Encounter
+    SET currentEnemy=iBandit
+    CALL "%cd%\data\Combat Engine\scripts\var_handler.bat"
+    IF %HP% LEQ 0 (
+        SET displayMessage=You wake up and find yourself safe within the walls of Windhelm.
+        GOTO :AUTOSAVE
+    ) ELSE (
+        GOTO :MAIN
+    )
+) ELSE IF %E% EQU 3 (
+    REM Encounter Nothing
+    SET displayMessage=You walk for several minutes, but find nothing.
+    GOTO :MAIN
+) ELSE IF %E% EQU 4 (
+    REM Find 13 Gold.
+    SET displayMessage=You stumble upon a coin purse alongside the path, it had 13 gold.
     SET /A COINS=!COINS! +13
-    GOTO :%LC%
+    GOTO :MAIN
+) ELSE IF %E% EQU 5 (
+    REM Memory Encounter
+    REM Not programmed.
+    GOTO :MAIN
+) ELSE IF %E% EQU 6 (
+    REM Shard Encounter
+    REM Not programmed.
+    GOTO :MAIN
+) ELSE (
+    REM Error handling.
+    SET errorType=Explore Error, encounter. >> EE-log.txt
 )
-
-REM 1 movement point North of ChunkPos1.
-:C1P1N
-REM If the movement points have been reset, go back to the "Main" direction to allow exploration of any direction.
-IF %C1P1NM% EQU 3 GOTO :ChunkPos1N
-SET LC=C1P1N
-CLS
-ECHO.
-TYPE "TITLE\PATH\HERE"
-ECHO.
-ECHO DEBUG: Current Pos: %C1P1NM%
-ECHO Where would you like to go?
-ECHO %displayMessage%
-ECHO =====================================================================================================
-ECHO [1 / GO NORTH ] ^| [X / GO SOUTH ] ^|[X / GO EAST ] ^| [X / GO WEST ] ^| [E / GO BACK ]
-CHOICE /C 1E /N /M ">"
-IF ERRORLEVEL 2 GOTO :C1PD
-IF ERRORLEVEL 1 GOTO :C1Nc
-
-REM "Go Back", adds +1 to C1P1NM.
-:C1PD
-SET /A C1P1NM=!C1P1NM! +1
-IF %C1P1NM% GTR 3 SET C1P1NM=3
-GOTO :ChunkPos1N
-
 
 REM Call SLOP and save the game before exiting Exploration Engine.
 :AUTOSAVE
