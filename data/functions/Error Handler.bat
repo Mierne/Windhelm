f49dba4d9bed972d63bbc4661e9f56a6af383f2e
Mierne <152667836+Mierne@users.noreply.github.com>
2023-12-13 13:13:52 -0500

    Updated Error Handler.bat
    - Added support for 1 new error type.

--- data/functions/Error Handler.bat
+++ data/functions/Error Handler.bat
@@ -1,13 +1,15 @@
 @ECHO OFF
-TITLE ERROR - %errorType% (WINDHELM 1.2.1 "Build 2")
+TITLE ERROR - %errorType%
 COLOR 4F
-REM Error Handler v1 231126.
+REM Error Handler v1.1 231213. For Windhelm Build 2 "Bottle o' Features"
 
 REM Check error type
 IF %errorType% == EnemyType (
-    GOTO enemyTypeError
+    GOTO :enemyTypeError
 ) ELSE IF %errorType% == checkTime (
     GOTO :ctError
+) ELSE IF %errorType% == attributeSkill (
+    GOTO :aSerror
 ) ELSE (
     GOTO :EOF
 )
@@ -20,5 +22,19 @@ ECHO ERROR! Error Type: EnemyType.
 ECHO =============================
 ECHO This could mean:
 ECHO The "currentEnemy" variable was left blank, or is unrecognized.
+PAUSE
+EXIT /B
+
+REM Display the "Attribute - Skill" error.
+:aSerror
+CLS
+ECHO.
+ECHO ERROR! Error Type: attributeSkill.
+ECHO Wuh-oh, Windhelm ran into a problem and can't continue!
+ECHO What'd you do this time?
+ECHO =======================================================
+ECHO This error could mean:
+ECHO The Player has modified their save, attempting to adjust Skill Levels to an undefined integer.
+ECHO This... this is the only way to cause this error. Stop messing with my shit!
 PAUSE
 EXIT /B
