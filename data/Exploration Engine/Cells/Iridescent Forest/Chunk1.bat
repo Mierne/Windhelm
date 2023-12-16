73e801c646ebbb28f707b5478d75522f3240155f
Mierne <152667836+Mierne@users.noreply.github.com>
2023-12-16 14:20:48 -0500

    Updated Chunk1.bat
    - Updated path to evie.bat
    - Added Gnome, Goblin and Hunter to Encounter list for Chunk 1.
    - Added extra logic to enounters.

--- data/Exploration Engine/Cells/Iridescent Forest/Chunk1.bat
+++ data/Exploration Engine/Cells/Iridescent Forest/Chunk1.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 TITLE Iridescent Forest (EXPLORE) - Chunk 1
-REM Chunk 1 (231202)
+REM Chunk 1 (231216)
 REM --
 REM Chunk 1 contains low-level enemies, such as the Bandit and Jester.
 REM This chunk also contains several NPCs important to the story, and will help guide the Player.
@@ -22,11 +22,7 @@ IF ERRORLEVEL 1 GOTO :MOVEMENT_CHECK
 
 REM Checks the Players HP to prevent instant-death in an encounter.
 :MOVEMENT_CHECK
-REM Debug move
-SET currentEnemy=iBandit
-CALL "%cd%\data\Combat Engine\scripts\var_handler.bat"
-REM - - Debug move end
-IF %HP% LSS 50 (
+IF %HP% LEQ 50 (
     GOTO :HPTL
 ) ELSE (
     GOTO :ENCOUNTER_CHECK
@@ -34,42 +30,65 @@ IF %HP% LSS 50 (
 
 REM Does the Player encounter an enemy or NPC?
 :ENCOUNTER_CHECK
+SET /A NOE=%RANDOM% %%4
+IF %NOE% GEQ 3 (
+    GOTO :ENCOUNTER_LIST
+) ELSE IF %NOE% LEQ 2 (
+    REM NPC Encounters, call a function to decide which NPC is encountered.
+    REM Unfinished.
+    REM Since this isn't ready - just redirect to Enemy Encounters.
+    GOTO :ENCOUNTER_LIST
+)
+
+:ENCOUNTER_LIST
+SET /A WE=%RANDOM% %%5
+IF %WE% EQU 1 (
+    REM Bandit Encounter.
+    SET currentEnemy=iBandit
+    CALL "%cd%\data\Combat Engine\scripts\evie.bat"
+    REM Return to Main Menu after battle is complete.
+    GOTO :MAIN
+) ELSE IF %WE% EQU 2 (
+    REM Jester Encounter.
+    SET currentEnemy=iJester
+    CALL "%cd%\data\Combat Engine\scripts\evie.bat"
+    REM Return to Main Menu after battle is complete.
+    GOTO :MAIN
+) ELSE IF %WE% EQU 3 (
+    REM Gnome Encounter.
+    SET currentEnemy=iGnome
+    CALL "%cd%\data\Combat Engine\scripts\evie.bat"
+    REM Return to Main Menu after battle is complete.
+    GOTO :MAIN
+) ELSE IF %WE% EQU 4 (
+    REM Hunter Encounter.
+    SET currentEnemy=iHunter
+    CALL "%cd%\data\Combat Engine\scripts\evie.bat"
+    REM Return to Main Menu after battle is complete.
+    GOTO :MAIN
+) ELSE IF %WE% EQU 5 (
+    REM Goblin Encounter.
+    SET currentEnemy=iGoblin
+    CALL "%cd%\data\Combat Engine\scripts\evie.bat"
+    REM Return to Main Menu after battle is complete.
+    GOTO :MAIN
+)
+
+REM Encountered NPCs are limited on a Cell by Cell basis.
+REM Which NPCs can be encountered in this Cell?
+:NPC_FIND
+SET /A WP=%RANDOM% %%3
+
 SET /A E=%RANDOM% %%6
 IF %E% EQU 1 (
-    REM Jester Encounter
-    SET currentEnemy=iJester
-    CALL "%cd%\data\Combat Engine\scripts\var_handler.bat"
-    IF %HP% LEQ 0 (
-        SET displayMessage=You wake up and find yourself safe within the walls of Windhelm.
-        GOTO :AUTOSAVE
-    ) ELSE (
-        GOTO :MAIN
-    )
-) ELSE IF %E% EQU 2 (
-    REM Bandit Encounter
-    SET currentEnemy=iBandit
-    CALL "%cd%\data\Combat Engine\scripts\var_handler.bat"
-    IF %HP% LEQ 0 (
-        SET displayMessage=You wake up and find yourself safe within the walls of Windhelm.
-        GOTO :AUTOSAVE
-    ) ELSE (
-        GOTO :MAIN
-    )
-) ELSE IF %E% EQU 3 (
     REM Encounter Nothing
     SET displayMessage=You walk for several minutes, but find nothing.
     GOTO :MAIN
-) ELSE IF %E% EQU 4 (
-    REM Find 13 Gold.
-    SET displayMessage=You stumble upon a coin purse alongside the path, it had 13 gold.
-    SET /A COINS=!COINS! +13
-    GOTO :MAIN
-) ELSE IF %E% EQU 5 (
-    REM Memory Encounter
+) ELSE IF %E% EQU 2 (
+    REM Shard Encounter.
     REM Not programmed.
-    GOTO :MAIN
-) ELSE IF %E% EQU 6 (
-    REM Shard Encounter
+) ELSE IF %E% EQU 3 (
+    REM Memory Encounter
     REM Not programmed.
     GOTO :MAIN
 ) ELSE (
