c2e490ea4e119c0a845299d78080bbbf5ad52886
Mierne <152667836+Mierne@users.noreply.github.com>
2023-12-18 16:28:49 -0500

    Updated Wizard.bat
    - Adjusted text in the Skill Points menu.
    - Added Affinity Discount logic.
    - Added Robes buy menu.

--- data/Exploration Engine/Cells/Windhelm High Quarter/NPCs/Wizard.bat
+++ data/Exploration Engine/Cells/Windhelm High Quarter/NPCs/Wizard.bat
@@ -1,6 +1,16 @@
 @ECHO OFF
 TITLE (Windhelm Castle) High Quarter - J'Zar Almani ^| %player_name% the %player_class%
-REM Wizard NPC v2 (231217) - for Windhelm Build 2 "Bottle o' Features"
+REM Wizard NPC v2 (231218) - for Windhelm Build 2 "Bottle o' Features"
+
+REM Check Affinity level and then enable/disable discounts.
+:CA
+IF %player_affinity_wizard% GEQ 400 (
+    SET wizDA=1
+    GOTO :MAIN_MENU
+) ELSE (
+    SET wizDA=0
+    GOTO :MAIN_MENU
+)
 
 :MAIN_MENU
 CLS
@@ -18,6 +28,149 @@ IF ERRORLEVEL 3 GOTO :SELL_ITEMS
 IF ERRORLEVEL 2 GOTO :SKILL_POINTS
 IF ERRORLEVEL 1 GOTO :BUY_ROBES
 
+REM Buy Robes
+:BUY_ROBES
+CLS
+ECHO type command goes here.
+ECHO +--------------------------------------------------------------------------------------------------+
+ECHO ^|                             Hm. Take a look, I have what you seek.                                +
+ECHO ^| %displayMessage%
+ECHO +--------------------------------------------------------------------------------------------------+
+ECHO + STATUS: %player_effect_1% , %player_effect_2%
+ECHO +--------------------------------------------------------------------------------------------------+
+ECHO ^| COINS: %coins% ^| AFFINITY: %player_affinity_wizard%
+ECHO +--------------------------------------------------------------------------------------------------+
+ECHO +                                          ITEM STOCK                                                +
+ECHO +--------------------------------------------------------------------------------------------------+
+ECHO ^| BASIC ROBES: %bRobes_s%, PRICE: %bRobes_s%
+ECHO ^| INTERMEDIATE ROBES: %iRobes_s%, PRICE: %iRobes_s%
+ECHO ^| ADVANDED ROBES: %aRobes_s%, PRICE: %aRobes_s%
+ECHO +--------------------------------------------------------------------------------------------------+
+ECHO + [1 / BASIC ROBES ] ^| [2 / INTERMEDIATE ROBES ] ^| [3 / ADVANDED ROBES ] ^| [E / BACK ]                +
+ECHO +--------------------------------------------------------------------------------------------------+
+CHOICE /C 123E /N /M ">"
+IF ERRORLEVEL 4 GOTO :MAIN_MENU
+IF ERRORLEVEL 3 GOTO :BUY_ADVANCED_ROBES
+IF ERRORLEVEL 2 GOTO :BUY_INTERMEDIATE_ROBES
+IF ERRORLEVEL 1 GOTO :BUY_BASIC_ROBES
+
+REM Purchase "Basic Robes".
+:BUY_BASIC_ROBES
+REM Check if Discounts are active.
+IF %wizDA% EQU 1 (
+    REM Check if the Player can afford the item.
+    IF %coins% LSS 460 (
+        REM The Player cannot afford this item.
+        SET displayMessage=No can do little Shard, you must pay...
+        GOTO :BUY_ROBES
+    ) ELSE (
+        REM Take the coins, remove [1] of the item from the Alchemist's stock and give [1] of the item to the Player.
+        SET /A coins=!coins! -%bRobes_p%
+        REM Apply the Discount.
+        SET /A coins=!coins! +%bRobes_affinityleveltwo_d%
+        REM Remove [1] of this item from the Merchant's stock.
+        SET /A bRobes_s=bRobes_s -1
+        REM Add [1] of this item to the Player's inventory.
+        SET /A bRobes_q=!bRobes_q! +1
+        SET displayMessage=Purchased 1 Basic Robe for 460 coins.
+        GOTO :BUY_ROBES
+    )
+) ELSE (
+    REM Check if the Player can afford the item.
+    IF %coins% LSS 500 (
+        REM The Player cannot afford this item.
+        SET displayMessage=No can do little Shard, you must pay...
+        GOTO :BUY_ROBES
+    ) ELSE (
+        REM Take the coins, remove [1] of the item from the Alchemist's stock and give [1] of the item to the Player.
+        SET /A coins=!coins! -%bRobes_p%
+        REM Remove [1] of this item from the Merchant's stock.
+        SET /A bRobes_s=!bRobes_s! -1
+        REM Add [1] of this item to the Player's inventory.
+        SET /A bRobes_q=!bRobes_q! +1
+        SET displayMessage=Purchased 1 Basic Robe for 500 coins.
+        GOTO :BUY_ROBES
+    )
+)
+
+REM Purchase "Intermediate Robes".
+:BUY_BASIC_ROBES
+REM Check if Discounts are active.
+IF %wizDA% EQU 1 (
+    REM Check if the Player can afford the item.
+    IF %coins% LSS 1100 (
+        REM The Player cannot afford this item.
+        SET displayMessage=No can do little Shard, you must pay...
+        GOTO :BUY_ROBES
+    ) ELSE (
+        REM Take the coins, remove [1] of the item from the Alchemist's stock and give [1] of the item to the Player.
+        SET /A coins=!coins! -%iRobes_p%
+        REM Apply the Discount.
+        SET /A coins=!coins! +%iRobes_affinityleveltwo_d%
+        REM Remove [1] of this item from the Merchant's stock.
+        SET /A iRobes_s=iRobes_s -1
+        REM Add [1] of this item to the Player's inventory.
+        SET /A iRobes_q=!iRobes_q! +1
+        SET displayMessage=Purchased 1 Intermediate Robe for 1100 coins.
+        GOTO :BUY_ROBES
+    )
+) ELSE (
+    REM Check if the Player can afford the item.
+    IF %coins% LSS 1200 (
+        REM The Player cannot afford this item.
+        SET displayMessage=No can do little Shard, you must pay...
+        GOTO :BUY_ROBES
+    ) ELSE (
+        REM Take the coins, remove [1] of the item from the Alchemist's stock and give [1] of the item to the Player.
+        SET /A coins=!coins! -%bRobes_p%
+        REM Remove [1] of this item from the Merchant's stock.
+        SET /A iRobes_s=!iRobes_s! -1
+        REM Add [1] of this item to the Player's inventory.
+        SET /A iRobes_q=!iRobes_q! +1
+        SET displayMessage=Purchased 1 Intermediate Robe for 1200 coins.
+        GOTO :BUY_ROBES
+    )
+)
+
+REM Purchase "Advanced Robes".
+:BUY_BASIC_ROBES
+REM Check if Discounts are active.
+IF %wizDA% EQU 1 (
+    REM Check if the Player can afford the item.
+    IF %coins% LSS 4800 (
+        REM The Player cannot afford this item.
+        SET displayMessage=No can do little Shard, you must pay...
+        GOTO :BUY_ROBES
+    ) ELSE (
+        REM Take the coins, remove [1] of the item from the Alchemist's stock and give [1] of the item to the Player.
+        SET /A coins=!coins! -%aRobes_p%
+        REM Apply the Discount.
+        SET /A coins=!coins! +%aRobes_affinityleveltwo_d%
+        REM Remove [1] of this item from the Merchant's stock.
+        SET /A aRobes_s=aRobes_s -1
+        REM Add [1] of this item to the Player's inventory.
+        SET /A aRobes_q=!aRobes_q! +1
+        SET displayMessage=Purchased 1 Advanced Robe for 4800 coins.
+        GOTO :BUY_ROBES
+    )
+) ELSE (
+    REM Check if the Player can afford the item.
+    IF %coins% LSS 5000 (
+        REM The Player cannot afford this item.
+        SET displayMessage=No can do little Shard, you must pay...
+        GOTO :BUY_ROBES
+    ) ELSE (
+        REM Take the coins, remove [1] of the item from the Alchemist's stock and give [1] of the item to the Player.
+        SET /A coins=!coins! -%aRobes_p%
+        REM Remove [1] of this item from the Merchant's stock.
+        SET /A aRobes_s=!aRobes_s! -1
+        REM Add [1] of this item to the Player's inventory.
+        SET /A aRobes_q=!aRobes_q! +1
+        SET displayMessage=Purchased 1 Advanced Robe for 5000 coins.
+        GOTO :BUY_ROBES
+    )
+)
+
 REM Apply skill points / upgrade specific stats.
 :SKILL_POINTS
 CLS
@@ -32,7 +185,7 @@ ECHO + Speech Skill: %speech_skill%
 ECHO +--------------------------------------------------------------------------------------------------+
 ECHO ^| COINS: %coins% ^| LEVELS: %levels% ^| AFFINITY: %player_affinity_wizard%
 ECHO +--------------------------------------------------------------------------------------------------+
-ECHO +                                          ITEM STOCK                                                +
+ECHO +                                  SKILL LEVEL INFORMATION                                           +
 ECHO +--------------------------------------------------------------------------------------------------+
 ECHO ^| DAMAGE +1, -20 LEVELS ^| STAMINA +1, -12 LEVELS ^| MAGICKA +1, -20 LEVELS                         +                 
 ECHO ^| SPEECH +1, -8 LEVELS                                                                              +
