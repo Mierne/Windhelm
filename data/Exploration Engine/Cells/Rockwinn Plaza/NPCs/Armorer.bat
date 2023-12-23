dd81ee9bb953e0d14ca37ebe4918a65a96a1d35c
Mierne <152667836+Mierne@users.noreply.github.com>
2023-12-23 13:31:00 -0500

    Updated Armorer.bat
    - Rewrote store logic.
    - Improved User Interface.

--- data/Exploration Engine/Cells/Rockwinn Plaza/NPCs/Armorer.bat
+++ data/Exploration Engine/Cells/Rockwinn Plaza/NPCs/Armorer.bat
@@ -1,218 +1,58 @@
 @ECHO OFF
-TITLE WINDHELM PLAZA - THE ARMORER
-REM Armorer v1.1 231028 - Windhelm v1.1b "Autumn Awakening"
+TITLE (Rockwinn Plaza) - The Armorer ^| Name: %player_name% ^| Class: %player_class%
+REM Armorer v2.0 231222 - For Windhelm Build 2 "Bottle 'o Features"
 
-REM MERCHANT DASHBOARD / MAIN MENU
-:dash
+REM Main Menu, used to access submenus.
+:MM
 CLS
-ECHO.
-TYPE "%cd%\data\ascii\npcs\armorer.txt"
-ECHO.
-ECHO                                           ARMORER
-ECHO                                 Hail, Shard. What can I do for you?
-ECHO.
-ECHO COINS: %coins% ^| AFFINITY: %player_affinity_armorer%
-ECHO %displayMessage%
-ECHO ========================================================================================================================
-ECHO [1] BUY ^| [T] TALK ^| [S] SELL ^| [E] LEAVE
-CHOICE /C 1TSE /N /M ">"
-IF ERRORLEVEL 4 GOTO :EOF
-IF ERRORLEVEL 3 GOTO :sell_armor
-IF ERRORLEVEL 2 GOTO :armorer_dialogue
-IF ERRORLEVEL 1 GOTO :buy_armor
-
-REM BUY ARMOR
-:buy_armor
-TITLE WP ARMOR - BUY ARMOR
-CLS
-ECHO.
-TYPE "%cd%\data\ascii\npcs\armorer.txt"
-ECHO.
-ECHO                                           ARMORER
-ECHO                                  I suppose I can spare some coin.
-ECHO.
-ECHO COINS: %coins%
-ECHO %displayMessage%
-ECHO ========================================================================================================================
-ECHO [1] CACTUS ARMOR ^| [2] STONE ARMOR ^| [3] STEEL ARMOR ^| [4] SCALED ARMOR ^| [E] BACK
-ECHO     50 COINS     ^|     100 COINS   ^|     570 COINS   ^|     900 COINS    ^|
-CHOICE /C 1234E /N /M ">"
-IF ERRORLEVEL 5 GOTO :dash
+ECHO +--------------------------------------------------------------------------------------------------+
+ECHO ^|                        Still hanging on to that mortal shell of yours?...                        +
+ECHO ^| %displayMessage%
+ECHO +--------------------------------------------------------------------------------------------------+
+ECHO + erm idk what to put here teehee...
+ECHO +--------------------------------------------------------------------------------------------------+
+ECHO ^| COINS: %coins% ^| AFFINITY: %player_affinity_armorer%
+ECHO +--------------------------------------------------------------------------------------------------+
+ECHO +                                          ITEM STOCK                                                +
+ECHO +--------------------------------------------------------------------------------------------------+
+ECHO ^|  CACTUS ARMOR: %cactusArmor_s%, PRICE: %cactusArmor_p% ^| SCALED ARMOR: %scaledArmor_s%, PRICE: %scaledArmor_p% ^| SILVER ARMOR: %silverArmor_s%, PRICE: %silverArmor_p%
+ECHO ^|  STONE ARMOR: %stoneArmor_s%, PRICE: %stoneArmor_p% ^| IRON ARMOR: %ironArmor_s%, PRICE: %ironArmor_p% ^| GOLD ARMOR: %goldArmor_s%, PRICE: %goldArmor_p%
+ECHO ^|  STEEL ARMOR: %steelArmor_s%, PRICE: %steelArmor_p% ^| LEATHER ARMOR: %leatherArmor_s%, PRICE: %leatherArmor_p% 
+ECHO +--------------------------------------------------------------------------------------------------+
+ECHO + [1 / CACTUS ARMOR ] ^| [2 / STONE ARMOR ] ^| [3 / STEEL ARMOR ] ^| [4 / SCALED ARMOR ]
+ECHO + [5 / IRON ARMOR ] ^| [6 / LEATHER ARMOR ] ^| [7 / SILVER ARMOR ] ^| [8 / GOLD ARMOR ]
+ECHO + [E / EXIT ]
+ECHO +--------------------------------------------------------------------------------------------------+
+CHOICE /C 12345678E /N /M ">"
+IF ERRORLEVEL 9 GOTO :EOF
+IF ERRORLEVEL 8 GOTO :buy_gold_armor
+IF ERRORLEVEL 7 GOTO :buy_silver_armor
+IF ERRORLEVEL 6 GOTO :buy_leather_armor
+IF ERRORLEVEL 5 GOTO :buy_iron_armor
 IF ERRORLEVEL 4 GOTO :buy_scaled_armor
-IF ERRORLEVEl 3 GOTO :buy_steel_armor
+IF ERRORLEVEL 3 GOTO :buy_steel_armor
 IF ERRORLEVEL 2 GOTO :buy_stone_armor
 IF ERRORLEVEL 1 GOTO :buy_cactus_armor
 
+REM Buy Cactus Armor store logic
 :buy_cactus_armor
-IF %player_affinity_armorer% GEQ 400 (
-    IF %coins% LSS %cactusAA1_price% (
-        GOTO :cannotAfford
+REM Check if the item is available.
+IF %cactusArmor_s% EQU 0 (
+    REM This item is out of stock.
+    SET displayMessage=Afraid I'm out of that item at the moment.
+    GOTO :MM
+) ELSE (
+    REM Check if the Player can afford this item.
+    IF %coins% LSS %cactusArmor_p% (
+        REM The Player cannot afford this item.
+        SET displayMessage=Sorry, you can't afford this.
+        GOTO :MM
     ) ELSE (
-        SET /A coins=!coins! -%cactusAA1_price%
+        REM Take [1] from the Merchant's stock, and add [1] to the Player's inventory, then deduct the price.
+        SET /A coins=!coins! -%cactusArmor_p%
+        SET /A cactusArmor_s=!cactusArmor_s! -1
         SET /A cactusA_q=!cactusA_q! +1
-        SET displayMessage=Purchased Cactus Armor for %cactusAA1_price%!
-        GOTO :dash
-    )
-) ELSE (
-    IF %coins% LSS %cactusA_price% (
-        GOTO :cannotAfford
-    ) ELSE (
-        SET /A coins=!coins! -%cactusA_price%
-        SET /A cactusA_q=!cactusA_q! +1
-        SET displayMessage=Purchased Cactus Armor for %cactusA_price%!
-        GOTO :dash
+        SET displayMessage=Purchased 1 Cactus Armor for %cactusArmor_p%.
+        GOTO :MM
     )
 )
-
-:buy_stone_armor
-IF %player_affinity_armorer% GEQ 400 (
-    IF %coins% LSS %stoneAA1_price% (
-        GOTO :cannotAfford
-    ) ELSE (
-        SET /A coins=!coins! -%stoneAA1_price%
-        SET /A stoneA_q=!stoneA_q! +1
-        SET displayMessage=Purchased Stone Armor for %stoneAA1_price%!
-        GOTO :dash
-    )
-) ELSE (
-    IF %coins% LSS %stoneA_price% (
-        GOTO :cannotAfford
-    ) ELSE (
-        SET /A coins=!coins! -%stoneA_price%
-        SET /A stoneA_q=!stoneA_q! +1
-        SET displayMessage=Purchased Stone Armor for %stoneA_price%!
-        GOTO :dash
-    )
-)
-
-:buy_steel_armor
-IF %player_affinity_armorer% GEQ 400 (
-    IF %coins% LSS %steelAA1_price% (
-        GOTO :cannotAfford
-    ) ELSE (
-        SET /A coins=!coins! -%steelAA1_price%
-        SET /A steelA_q=!steelA_q! +1
-        SET displayMessage=Purchased Steel Armor for %steelAA1_price%!
-        GOTO :dash
-    )
-) ELSE (
-    IF %coins% LSS %steelA_price% (
-        GOTO :cannotAfford
-    ) ELSE (
-        SET /A coins=!coins! -%steelA_price%
-        SET /A steelA_q=!steelA_q! +1
-        SET displayMessage=Purchased Steel Armor for %steelA_price%!
-        GOTO :dash
-    )
-)
-
-:buy_scaled_armor
-IF %player_affinity_armorer% GEQ 400 (
-    IF %coins% LSS %scaledAA1_price% (
-        GOTO :cannotAfford
-    ) ELSE (
-        SET /A coins=!coins! -%scaledAA1_price%
-        SET /A scaledA_q=!scaledA_q! +1
-        SET displayMessage=Purchased Scaled Armor for %scaledAA1_price%!
-        GOTO :dash
-    )
-) ELSE (
-    IF %coins% LSS %cactusA_price% (
-        GOTO :cannotAfford
-    ) ELSE (
-        SET /A coins=!coins! -%cactusA_price%
-        SET /A cactusA_q=!cactusA_q! +1
-        SET displayMessage=Purchased Cactus Armor for %cactusA_price%!
-        GOTO :dash
-    )
-)
-
-
-REM SELL ARMOR
-:sell_armor
-TITLE WP ARMORER - SELL ARMOR
-CLS
-ECHO.
-TYPE "%cd%\data\ascii\npcs\armorer.txt"
-ECHO.
-ECHO                                           ARMORER
-ECHO                                  I suppose I can spare some coin.
-ECHO.
-ECHO COINS: %coins%
-ECHO %displayMessage%
-ECHO ========================================================================================================================
-ECHO [1] CACTUS ARMOR ^| [2] STONE ARMOR ^| [3] STEEL ARMOR ^| [4] SCALED ARMOR ^| [5] GUARD ARMOR ^| [E] BACK
-ECHO     45 COINS     ^|     90 COINS    ^|     400 COINS   ^|     700 COINS    ^|     1200 COINS  ^|
-ECHO                        [E] EXIT
-CHOICE /C 12345E /N /M ">"
-IF ERRORLEVEL 6 GOTO :dash
-IF ERRORLEVEL 5 GOTO :sell_guard_armor
-IF ERRORLEVEL 4 GOTO :sell_scaled_armor
-IF ERRORLEVEL 3 GOTO :sell_steel_armor
-IF ERRORLEVEL 2 GOTO :sell_stone_armor
-IF ERRORLEVEL 1 GOTO :sell_cactus_armor
-
-REM Sell Armor functions.
-:sell_cactus_armor
-IF %cactusA_q% EQU 0 (
-    GOTO :no_item
-) ELSE (
-    ECHO SOLD CACTUS >> output.log
-    SET /A cactusA_q=!cactusA_q! -1
-    SET /A coins=!coins! +45
-    SET displayMessage=Sold Cactus armor for 45 coins!
-    GOTO :sell_armor
-)
-
-:sell_stone_armor
-IF %stoneA_q% EQU 0 (
-    GOTO :no_item
-) ELSE (
-    SET /A stoneA_q=!stoneA_q! -1
-    SET /A coins=!coins! +90
-    SET displayMessage=Sold Stone armor for 90 coins!
-    GOTO :sell_armor
-)
-
-:sell_steel_armor
-IF %steelA_q% EQU 0 (
-    GOTO :no_item
-) ELSE (
-    SET /A steelA_q=!steelA_q! -1
-    SET /A coins=!coins! +400
-    SET displayMessage=Sold Steel armor for 400 coins!
-    GOTO :sell_armor
-)
-
-:sell_scaled_armor
-IF %scaledA_q% EQU 0 (
-    GOTO :no_item
-) ELSE (
-    SET /A scaledA_q=!scaledA_q! -1
-    SET /A coins=!coins! +700
-    SET displayMessage=Sold Scaled armor for 700 coins!
-    GOTO :sell_armor
-)
-
-:sell_guard_armor
-IF %guardA_q% EQU 0 (
-    GOTO :no_item
-) ELSE (
-    SET /A guardA_q=!cactusA_q! -1
-    SET /A coins=!coins! +1200
-    SET displayMessage=Sold Guard armor for 1200 coins!
-    GOTO :sell_armor
-)
-
-REM "No Item" error message.
-:no_item
-SET displayMessage=You can't sell something you don't have!
-GOTO :sell_armor
-
-:cannotAfford
-SET displayMessage=You cannot afford this item.
-GOTO :buy_armor
-
-REM Not implemented.
-:armorer_dialogue
-GOTO :dash
